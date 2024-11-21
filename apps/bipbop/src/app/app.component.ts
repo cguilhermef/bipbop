e30525a21ad9e3e6fc65f6c6718b63e0e31ee7de
Christian G. Fritsch <christiangfritsch@gmail.com>
2024-11-21 10:35:34 -0300

    feat: cria menu principal e layout básico

--- apps/bipbop/src/app/app.component.ts
+++ apps/bipbop/src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
